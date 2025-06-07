b6daba5c23265b28e971cc411eabda6fe3549954
SrinivasD <115702816+aceducky@users.noreply.github.com>
2025-06-07 16:01:39 +0530

    use express.json to parse data

--- express_/server.js
+++ express_/server.js
@@ -1,5 +1,17 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+// console.log(app);
 
-console.log(app);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send(`Got post req with body: \n${JSON.stringify(req.body,null,2)}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server: http://localhost:${PORT}`);
+});

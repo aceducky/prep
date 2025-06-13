e3ac50c73e83b43ee43187eceb79e0f193f7937b
SrinivasD <115702816+aceducky@users.noreply.github.com>
2025-06-13 14:50:29 +0530

    semi commit

---
+++ express_/auth.js
@@ -0,0 +1,68 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import cors from "cors";
+
+const app = express();
+
+const port = 5000;
+const ALL_USERS = [
+  {
+    username: "harkirat@gmail.com",
+    password: "123",
+    name: "harkirat singh",
+  },
+  {
+    username: "raman@gmail.com",
+    password: "123321",
+    name: "Raman singh",
+  },
+  {
+    username: "priya@gmail.com",
+    password: "123321",
+    name: "Priya kumari",
+  },
+];
+
+function userExists(username, password) {
+  // write logic to return true or false if this user exists
+  // in ALL_USERS array
+  return (
+    ALL_USERS.findIndex(
+      (user) => user.username === username && user.password === password
+    ) !== -1
+  );
+}
+
+app.post("/signin", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!userExists(username, password)) {
+    return res.status(403).json({
+      msg: "User doesnt exist in our in memory db",
+    });
+  }
+
+  const token = jwt.sign({ username: username }, "shhhhh");
+  return res.json({
+    token,
+  });
+});
+
+app.get("/users", function (req, res) {
+  const token = req.headers.authorization;
+  try {
+    const decoded = jwt.verify(token, jwtPassword);
+    const username = decoded.username;
+    // return a list of users other than this username
+  } catch (err) {
+    return res.status(403).json({
+      msg: "Invalid token",
+    });
+  }
+});
+
+
+app.listen(port, () => {
+  console.log("Running on http://localhost:5000");
+});
--- express_/server.js
+++ express_/server.js
@@ -16,48 +16,70 @@ app.use(
     origin: (origin, cb) => {
       if (process.env.NODE_ENV.toLowerCase() !== "development") {
         console.error(
-          "[CORS ERROR] Change cors options for this environment (NODE_ENV)\nCurrently using development environment's cors options\nThis is NOT safe."
+          "[CORS ERROR] Change cors options for this environment (NODE_ENV)\nCurrently using development environment's cors options\nThis is NOT safe. origin: ",
+          origin
         );
         cb(new Error("Not allowed by CORS (development)"));
       } else if (process.env.NODE_ENV.toLowerCase() === "development") {
+        console.log("Allowed request from origin: ", origin);
         cb(null, true);
       } else if (origin && allowedOrigins.includes(origin)) {
+        console.log("Allowed request from origin: ", origin);
         cb(null, true);
       } else {
-        cb(new Error("Not allowed by CORS"));
+        cb(new Error("Not allowed by CORS, origin: ", origin));
       }
     },
     credentials: true,
   })
 );
 
-app.use(express.text());
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/text-body", (req, res) => {
-  console.log(req.body);
+app.post("/text-body", express.text(),(req, res) => {
   res.send(`Got post req with body:\n${JSON.stringify(req.body, null, 2)}`);
 });
 
 app.post("/json-body", (req, res) => {
-  console.log(req.body);
   res.send(`Got post req with body:\n${JSON.stringify(req.body, null, 2)}`);
-});
+  });
 
 app.get("/some-html", (_req, res) => {
   res.send("<h2>some html</h2>");
 });
 
 app.post("/sum-of-numbers", (req, res) => {
-  const schema = z.array(z.number());
-  const numbers = req.body.numbers;
-  const response = schema.safeParse(numbers);
-  res.send({ response });
+  const schema = z.object({
+    numbers: z.array(z.number()).min(0).max(10),
+  });
+
+  const response = schema.safeParse(req.body);
+  if (!response.success) {
+    return res.status(400).json({
+      status: 400,
+      message: "Error: numbers must be an array of numbers.",
+      error: response.error.issues,
+    });
+  }
+
+  const sum = response.data.numbers.reduce((acc, num) => acc + num, 0);
+  res.status(200).json({ sum });
 });
 
-// Global error catch
-app.use((err, req, res, next) => {
+app.use((err, _req, res, _next) => {
+  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+    console.error("Invalid JSON Payload:", err.message);
+    return res.status(400).json({
+      status: 400,
+      message: "Invalid JSON payload",
+    });
+  }
+
   console.error("Global Catch:", err.message || err);
   res.status(500).json({ msg: "Something went wrong" });
 });
